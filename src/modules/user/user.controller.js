3493eb0c67ee223a44f0074344e5bc6712ca01f0
Amine Trigui <aminetrigui.github@gmail.com>
2024-04-23 13:58:54 +0100

    bcrypt

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,9 +1,14 @@
-import { userModel } from "../../../models/user.models.js"
+import bcrypt from "bcrypt"
+import { userModel } from "../../../models/user.models.js";
 
-export const signUp = async(req, res) => { 
-  const {name,email,password } = req.body
+export const signUp = async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await  userModel.findOne({email})
+  if (user) 
+  {res.json("user already exist") } 
+  else { const hash =  bcrypt.hashSync(password,17)
+    await userModel.insertMany({ name, email, password: hash });
+  res.json({ message: "User added with success" });
+  }
 
-await userModel.insertMany({ name, email, password })
-res.json({message: "User added with success"})
- }
-
+};
